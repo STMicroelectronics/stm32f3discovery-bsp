7da5d8482b5ec91e15994068dc6702a32b2b3f77
Tasnim <tasnim.ouni@st.com>
2023-03-10 10:33:33 +0100

    Release v2.1.7

--- stm32f3_discovery_accelerometer.h
+++ stm32f3_discovery_accelerometer.h
@@ -2,38 +2,21 @@
   ******************************************************************************
   * @file    stm32f3_discovery_accelerometer.h
   * @author  MCD Application Team
-  * @brief   This file contains definitions for stm32f3_discovery_accelerometer.c 
+  * @brief   This file contains definitions for stm32f3_discovery_accelerometer.c
   *          firmware driver.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  * Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
-  
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F3_DISCOVERY_ACCELERO_H
@@ -47,19 +30,20 @@
 #include "stm32f3_discovery.h"
 /* Include Gyroscope component driver */
 #include "../Components/lsm303dlhc/lsm303dlhc.h"
-   
+#include "../Components/lsm303agr/lsm303agr.h"
+
 /** @addtogroup BSP
   * @{
   */
-  
+
 /** @addtogroup STM32F3_DISCOVERY
   * @{
-  */ 
+  */
 
 /** @addtogroup STM32F3_DISCOVERY_ACCELEROMETER
   * @{
   */
-  
+
 /** @defgroup STM32F3_DISCOVERY_ACCELEROMETER_Exported_Types Exported Types
   * @{
   */
@@ -67,22 +51,22 @@
 /**
   * @}
   */
-  
+
 /** @defgroup STM32F3_DISCOVERY_ACCELEROMETER_Exported_Constants Exported Constants
   * @{
   */
-typedef enum 
+typedef enum
 {
   ACCELERO_OK = 0,
   ACCELERO_ERROR = 1,
   ACCELERO_TIMEOUT = 2
-} 
+}
 ACCELERO_StatusTypeDef;
 
 /**
   * @}
   */
-  
+
 /** @defgroup STM32F3_DISCOVERY_ACCELEROMETER_Exported_Macros Exported Macros
   * @{
   */
@@ -95,7 +79,7 @@ ACCELERO_StatusTypeDef;
 /** @defgroup STM32F3_DISCOVERY_ACCELEROMETER_Exported_Functions Exported Functions
   * @{
   */
-/* Acc functions */  
+/* Acc functions */
 uint8_t   BSP_ACCELERO_Init(void);
 void      BSP_ACCELERO_Reset(void);
 void      BSP_ACCELERO_GetXYZ(int16_t *pDataXYZ);
@@ -114,10 +98,9 @@ void      BSP_ACCELERO_GetXYZ(int16_t *pDataXYZ);
 
 /**
   * @}
-  */ 
+  */
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __STM32F3_DISCOVERY_ACCELERO_H */
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/ 
--- stm32f3_discovery_gyroscope.h
+++ stm32f3_discovery_gyroscope.h
@@ -2,38 +2,21 @@
   ******************************************************************************
   * @file    stm32f3_discovery_gyroscope.h
   * @author  MCD Application Team
-  * @brief   This file contains definitions for stm32f3_discovery_gyroscope.c 
+  * @brief   This file contains definitions for stm32f3_discovery_gyroscope.c
   *          firmware driver.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  * Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
-  
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F3_DISCOVERY_GYRO_H
@@ -46,34 +29,35 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f3_discovery.h"
 /* Include Gyroscope component driver */
-#include "../Components/l3gd20/l3gd20.h"   
+#include "../Components/l3gd20/l3gd20.h"
+#include "../Components/i3g4250d/i3g4250d.h"
 
 /** @addtogroup BSP
   * @{
   */
-  
+
 /** @addtogroup STM32F3_DISCOVERY
   * @{
-  */ 
+  */
 
 /** @addtogroup STM32F3_DISCOVERY_GYROSCOPE
   * @{
   */
-  
+
 /** @defgroup STM32F3_DISCOVERY_GYROSCOPE_Exported_Types Exported Types
   * @{
   */
-typedef enum 
+typedef enum
 {
   GYRO_OK = 0,
   GYRO_ERROR = 1,
   GYRO_TIMEOUT = 2
-} 
+}
 GYRO_StatusTypeDef;
 
 /**
   * @}
-  */ 
+  */
 
 /** @defgroup STM32F3_DISCOVERY_GYROSCOPE_Exported_Constants Exported Constants
   * @{
@@ -82,7 +66,7 @@ GYRO_StatusTypeDef;
 /**
   * @}
   */
-  
+
 /** @defgroup STM32F3_DISCOVERY_GYROSCOPE_Exported_Macros Exported Macros
   * @{
   */
@@ -90,11 +74,11 @@ GYRO_StatusTypeDef;
 /**
   * @}
   */
- 
+
 /** @defgroup STM32F3_DISCOVERY_GYROSCOPE_Exported_Functions  Exported Functions
   * @{
   */
-/* Sensor Configuration Functions */ 
+/* Sensor Configuration Functions */
 uint8_t BSP_GYRO_Init(void);
 void BSP_GYRO_Reset(void);
 uint8_t BSP_GYRO_ReadID(void);
@@ -117,12 +101,11 @@ void BSP_GYRO_GetXYZ(float* pfData);
 
 /**
   * @}
-  */ 
-  
+  */
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __STM32F3_DISCOVERY_GYRO_H */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/ 
